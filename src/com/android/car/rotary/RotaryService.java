9c47c404e238cd06e115e7dc1b8246153b83f356
Yabin Huang <yabinh@google.com>
2026-01-20 23:33:36 +0000

    Reset rotary_input_method if the specified IME is not installed
    This change prevents CarRotaryImeTest failures on branches where the reference rotary IME is not included in the build.
    
    AOSP config.xml specifies R.string.rotary_input_method, but the actual IME APK is not present in older branches, causing CTS failures. This has been corrected on the main branch.
    
    To address this on older branches, this CL checks if the specified rotary_input_method corresponds to an installed IME in RotaryService. If the IME is not installed, it resets the value to an empty string.
    
    This approach was chosen over:
    1.  Cherry-picking main branch fixes: Deemed too risky due to the number and size of CLs involved.
    2.  Adding the rotary IME APK to old branches: Requires significant effort to set up new repositories.
    
    This solution ensures no functional regressions or CTS test failures for OEMs. The reference rotary IME is not used by any OEMs so far, and OEMs who do use a dedicated rotary IME are expected to include the necessary APK in their build.
    
    Bug: 407824381
    Fixes: 397795041
    Test: atest CtsCarTestCases
    
    Change-Id: I77d2bcce776ab03aa6ceb953ecffbc2a1dc0f246

--- src/com/android/car/rotary/RotaryService.java
+++ src/com/android/car/rotary/RotaryService.java
@@ -630,6 +630,13 @@ public class RotaryService extends AccessibilityService implements
         }
 
         mRotaryInputMethod = res.getString(R.string.rotary_input_method);
+        if (!TextUtils.isEmpty(mRotaryInputMethod)
+                && !Utils.isInstalledIme(mRotaryInputMethod, mInputMethodManager)) {
+            L.e("Rotary IME is specified to " + mRotaryInputMethod + " but not installed. "
+                    + "If you're sure you need a dedicated rotary IME, please install the specified"
+                    + " apk; otherwise please don't overload the config rotary_input_method");
+            mRotaryInputMethod = "";
+        }
         mDefaultTouchInputMethod = getDefaultTouchInputMethod(res, mInputMethodManager);
         if (mDefaultTouchInputMethod == null) {
             throw new IllegalStateException("No touch IME installed");
